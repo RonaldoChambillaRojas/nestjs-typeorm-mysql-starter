1f967d439c2ca5981055ab137fe93354faf27df5
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2026-01-07 11:27:35 -0500

    fix(app): update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { validate } from './config/env.validation';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get<TypeOrmModuleOptions>('database')!,
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
